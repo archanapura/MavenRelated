a100a63fb59c1e4d9326aec15d60374a02a7fbb0
archanapura <archanapura@gmail.com>
2021-05-22 18:50:50 +0530

    maven code

---
+++ src/test/java/com/tests/TestCase1.java
@@ -0,0 +1,14 @@
+package com.tests;
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+
+    @Test
+    public void demo()
+    {
+        //String str=System.getProperty("browser");
+        System.out.println("Hello");
+      //  System.out.println(str);
+    }
+
+}
